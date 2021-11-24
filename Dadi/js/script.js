53e00ccf04dc03f3bb452fc7984f00d7ff12762c
filauro1989 <filauro1989@gmail.com>
2021-11-24 15:01:49 +0100

    dadi

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,12 +1,35 @@
-let dadoUtente = Math.floor(Math.random() * 6) + 1;
-let dadoPc = Math.floor(Math.random() * 6) + 1;
+let button = document.querySelector('.tira-dadi');
+let resultBox = document.querySelector('.result-box');
+let userBox = document.getElementById('user-box');
+let pcBox = document.getElementById('pc-box');
 
-if (dadoUtente > dadoPc) {
-    console.log('Bravo, hai vinto!');
-} else if (dadoUtente < dadoPc) {
-    console.log('Peccato, hai perso!');
-} else {
-    console.log('Pareggio!');
-}
-console.log(dadoUtente);
-console.log(dadoPc);
+button.addEventListener('click', function() {
+
+    let dadoUtente = Math.floor(Math.random() * 6) + 1;
+    let dadoPc = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("risultato").innerHTML = '';
+    userBox.innerHTML = dadoUtente;
+    pcBox.innerHTML = dadoPc;
+
+
+    if (dadoUtente > dadoPc) {
+        console.log('Bravo, hai vinto!');
+        const result = document.createTextNode("Hai vinto!");
+        resultBox.append(result);
+
+    } else if (dadoUtente < dadoPc) {
+        console.log('Peccato, hai perso!');
+        const result = document.createTextNode("Hai perso!");
+        resultBox.append(result);
+    } else {
+        console.log('Pareggio!');
+        const result = document.createTextNode("Pareggio!");
+        resultBox.append(result);
+    }
+    console.log(dadoUtente);
+    console.log(dadoPc);
+
+})
+
+
+
